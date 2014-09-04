7a44591921db72c60f45197b3f33018e43de9e9f
reflexdemon <venkateswara.venkatraman@cbeyond.net>
2014-09-03 23:06:15 -0400

    Made some tweeks to main method. This is very useful for my remote ssh project

--- generex/src/com/mifmif/common/regex/Main.java
+++ generex/src/com/mifmif/common/regex/Main.java
@@ -19,38 +19,53 @@ package com.mifmif.common.regex;
 
 import java.util.List;
 
-import com.mifmif.common.regex.util.Iterator;
-
 /**
  * @author y.mifrah
- *
+ * 
  */
 public class Main {
-	public static void main(String[] args) {
-		Generex generex = new Generex("[0-3]([a-c]|[e-g]{1,2})");
+    public static void main(String[] args) {
+        String raw = "user@server[0-8][0-8]env.sub.domain.sufix";
+        String parse = parseRawInput(raw);
+        Generex generex = new Generex(parse);
+        
+        generex.matchedStringsSize();
 
-		// generate the second String in lexicographical order that match the
-		// given Regex.
-		String secondString = generex.getMatchedString(2);
-		System.out.println(secondString);// it print '0b'
 
-		// Generate all String that matches the given Regex.
-		List<String> matchedStrs = generex.getAllMatchedStrings();
+        // Generate all String that matches the given Regex.
+        List<String> matchedStrs = generex.getAllMatchedStrings();
 
-		// Using Generex iterator
-		Iterator iterator = generex.iterator();
-		while (iterator.hasNext()) {
-			System.out.print(iterator.next() + " ");
-		}
-		// it print 0a 0b 0c 0e 0ee 0e 0e 0f 0fe 0f 0f 0g 0ge 0g 0g 1a 1b 1c 1e
-		// 1ee
-		// 1e 1e 1f 1fe 1f 1f 1g 1ge 1g 1g 2a 2b 2c 2e 2ee 2e 2e 2f 2fe 2f 2f 2g
-		// 2ge 2g 2g 3a 3b 3c 3e 3ee 3e 3e 3f 3fe 3f 3f 3g 3ge 3g 3g 1ee
+        for (String str : matchedStrs) {
+            System.out.println(str);
+        }
 
-		// Generate random String
-		String randomStr = generex.random();
-		System.out.println(randomStr);// a random value from the previous String
-										// list
+    }
 
-	}
+    /**
+     * Parses the raw input.
+     *
+     * @param raw the raw
+     * @return the string
+     */
+    private static String parseRawInput(String raw) {
+        System.out.println("RAW : " + raw);
+        StringBuilder build = new StringBuilder();
+        for (char c : raw.toCharArray()) {
+            switch (c) {
+            /*
+             * These are special cases that needs to be escaped. If these chars
+             * are allowed this would hang the regexp generator.
+             */
+                case '@':
+                case '.':
+                    build.append("\\").append(c);
+                    break;
+                default:
+                    build.append(c);
+
+            }
+        }
+        System.out.println("PARSED: " + build);
+        return build.toString();
+    }
 }
